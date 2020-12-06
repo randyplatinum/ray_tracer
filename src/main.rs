3afe6b1ad71af85a878b56648cbdce3bd4c85600
Joe Wilder <j.m.wilder715@gmail.com>
2020-12-05 19:14:40 -0500

    created .ppm equivalent of 'hello world'

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+const IMAGE_HEIGHT: u8=u8::MAX;
+const IMAGE_WIDTH: u8=u8::MAX;
+
 fn main() {
-    println!("Hello, world!");
+    println!("P3\n{} {}\n255", IMAGE_HEIGHT, IMAGE_WIDTH);
+    for i in (0..IMAGE_HEIGHT).rev()
+    {
+    	//println!("?");
+    	for j in 0..IMAGE_WIDTH
+    	{
+    		let r = i as f64 / (IMAGE_WIDTH as f64 - 1f64);
+    		let g = j as f64 / (IMAGE_HEIGHT as f64 - 1f64);
+    		let b = 0.25f64;
+
+    		let ir = (255.999 * r) as u32;
+    		let ig = (255.999 * g) as u32;
+    		let ib = (255.999 * b) as u32;
+
+    		println!("{} {} {}", ir, ig, ib);
+    	}
+    }
 }
